f7334fa3a71df3f74b35a48fedfbd329dcddbe88
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2020-12-10 22:19:46 -0500

    Created TurnByGyro Auto Command

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/auto/TurnByGyro.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.robot.commands.auto;
+
+import com.disnodeteam.dogecommander.Command;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.robot.subsystems.Drive;
+
+public class TurnByGyro implements Command {
+    private Drive drive;
+
+    private ElapsedTime timer;
+    private double timeout;
+    private double power;
+
+    private double target;
+
+    public TurnByGyro(Drive drive, double degrees, double power, double timeout){
+        this.drive = drive;
+        this.target = degrees;
+        this.power = power;
+        this.timeout = timeout;
+    }
+
+    @Override
+    public void start() {
+        timer.reset();
+    }
+
+    @Override
+    public void periodic() {
+        drive.setPower(
+                 power * (target - drive.heading() > 0 ? 1 : -1),
+                power * (target - drive.heading() < 0 ? 1 : -1));
+    }
+
+    @Override
+    public void stop() {
+        drive.setPower(0,0);
+    }
+
+    @Override
+    public boolean isCompleted() {
+
+        return Range.clip(drive.heading(),target+1,target-1) == drive.heading() || timer.seconds() > timeout;
+    }
+}
