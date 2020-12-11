17ce8c28024ef0b31f52647534de585422e40edb
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2020-12-10 22:20:59 -0500

    Added timeout for Kicker in Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/auto/KickerSetState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/auto/KickerSetState.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.robot.commands.auto;
 
 import com.disnodeteam.dogecommander.Command;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.robot.subsystems.Kicker;
 
@@ -8,21 +9,26 @@ public class KickerSetState implements Command {
     private Kicker kicker;
 
     private double state;
+    private double timeout;
+    private ElapsedTime timer;
 
-    public KickerSetState(Kicker kicker, double state){
+    public KickerSetState(Kicker kicker, double state, double timeout){
         this.kicker = kicker;
         this.state = state;
+        this.timeout = timeout;
     }
 
-    public KickerSetState(Kicker kicker, boolean toggle){
+    public KickerSetState(Kicker kicker, boolean toggle, double timeout){
         this.kicker = kicker;
         this.state = -1;
+        this.timeout = timeout;
 
         kicker.togglePos();
     }
 
     @Override
     public void start() {
+        timer.reset();
         if(state != -1) kicker.setTargetPos(state);
     }
 
@@ -38,6 +44,6 @@ public class KickerSetState implements Command {
 
     @Override
     public boolean isCompleted() {
-        return kicker.getTargetPos() == kicker.getCurrentPos();
+        return kicker.getTargetPos() == kicker.getCurrentPos() || timer.seconds() > timeout;
     }
 }
