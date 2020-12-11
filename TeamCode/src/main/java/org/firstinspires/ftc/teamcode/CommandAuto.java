0d8b30ef7a7ce744fa06229abbea90f58e6a1c0a
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2020-12-10 22:21:51 -0500

    Made a sketch for Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandAuto.java
@@ -5,7 +5,13 @@ import com.disnodeteam.dogecommander.DogeOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.robot.commands.auto.ArmByEncoder;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.DriveByEncoder;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.DriveByTimer;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.GripSetState;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.KickerSetState;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.RunShooterForTime;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.TurnByGyro;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Drive;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Grip;
@@ -41,13 +47,57 @@ public class CommandAuto extends LinearOpMode implements DogeOpMode {
         // Run after init but before start
         while (!isStarted()){
             //TODO: Camera implementation
-            telemetry.addLine("Camera in progress");
+            telemetry.addLine("Ready!");
+            telemetry.addLine("Camera in progress...");
             telemetry.update();
         }
-        
 
-        commander.runCommand(new DriveByTimer(drive,3,0.3));
+        commander.runCommandsParallel(
+                new RunShooterForTime(shooter,-1,.3),                              // Turn on shooter
+                new DriveByEncoder(drive,InchToCount(55),0,.3,10),                 // Drive to line
+                new ArmByEncoder(arm,Arm.TARGETS.UP.getTarget(),0,.3,2),    // Bring wobble arm up
+                new GripSetState(grip,Grip.TARGETS.CLOSE.getTarget()));
+
+        sleep(1000);
+
+        //Shot 1
+        commander.runCommand(new KickerSetState(kicker,true,1));
+        sleep(500);
+        commander.runCommand(new KickerSetState(kicker,true,1));
+
+        //Shot 2
+        sleep(500);
+        commander.runCommand(new KickerSetState(kicker,true,1));
+        sleep(500);
+        commander.runCommand(new KickerSetState(kicker,true,1));
+
+        //Shot 3
+        sleep(500);
+        commander.runCommand(new KickerSetState(kicker,true,1));
+
+        commander.runCommandsParallel(
+                new RunShooterForTime(shooter,0,.3), // Turn off shooter
+                new TurnByGyro(drive,30,.3,2) // Turn to face drop zone
+                );
+        sleep(3000);
+
+        // Drive in front of drop zone
+        commander.runCommand(new DriveByEncoder(drive,InchToCount(10),0,0.3,5));
+        sleep(1000);
+
+        // Put arm out
+        commander.runCommand(new ArmByEncoder(arm,Arm.TARGETS.OUT.getTarget(),0,.3,2));
+        sleep(200);
+
+        // Open grip to drop Wobble Goal
+        commander.runCommand(new GripSetState(grip,true));
+        sleep(300);
+
+        // Drive back to line
+        commander.runCommand(new DriveByTimer(drive,0.5,-.7));
 
         commander.stop();
     }
+
+    private int InchToCount(int count){return (int)(count*DriveByEncoder.COUNTS_PER_INCH_EMPIRICAL);}
 }
