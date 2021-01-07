9c3f6ebc9ddae456bf649b6b33dde813bc448726
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2021-01-06 23:20:47 -0500

    Explicently stated Transfer's direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/auto/RunTransferForTime.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/auto/RunTransferForTime.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.robot.commands.auto;
 
 import com.disnodeteam.dogecommander.Command;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.robot.subsystems.Transfer;
@@ -23,7 +24,7 @@ public class RunTransferForTime implements Command {
     @Override
     public void start() {
         timer.reset();
-        transfer.setReverse(false);
+        transfer.setDirection(DcMotorSimple.Direction.FORWARD);
         transfer.setPower(power);
     }
 
