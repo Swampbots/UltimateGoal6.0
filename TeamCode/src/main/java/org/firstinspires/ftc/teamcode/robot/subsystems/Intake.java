8725da869d1106d0b3a91e3a4d244f2b19914b18
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2021-01-06 23:05:17 -0500

    Cleaned up Intake Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Intake.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.robot.subsystems;
 
 import com.disnodeteam.dogecommander.Subsystem;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Intake implements Subsystem {
@@ -9,39 +10,41 @@ public class Intake implements Subsystem {
     private DcMotor intake;
 
     private double power = 0;
-    private boolean reverse = false;
 
-    public Intake(HardwareMap hardwareMap){
+    public Intake(HardwareMap hardwareMap) {
         this.hardwareMap = hardwareMap;
     }
 
     @Override
     public void initHardware() {
         intake = hardwareMap.get(DcMotor.class,"intake");
+        intake.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     @Override
     public void periodic() {
-        intake.setPower(power * (reverse?-1:1));
+        intake.setPower(power);
     }
 
-    public void setPower(double power){
-        this.power = power;
-    }
-
-    public double getPower(){
+    public double getPower() {
         return power;
     }
 
-    public void setReverse(boolean reverse){
-        this.reverse = reverse;
+    public DcMotorSimple.Direction getDirection() {
+        return intake.getDirection();
     }
 
-    public void reverse(){
-        this.reverse = !this.reverse;
+    public void setPower(double power) {
+        this.power = power;
     }
 
-    public boolean getReverse(){
-        return reverse;
+    public void setDirection(DcMotorSimple.Direction direction) {
+        intake.setDirection(direction);
     }
+
+    public void reverse() {
+        if(intake.getDirection() == DcMotorSimple.Direction.FORWARD) intake.setDirection(DcMotorSimple.Direction.REVERSE);
+        else intake.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
 }
