3cc652fef3a935f7fa0adbefd6f673bfd761db73
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2021-01-06 23:05:33 -0500

    Cleaned up Shooter Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Shooter.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.robot.subsystems;
 
-
 import com.disnodeteam.dogecommander.Subsystem;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Shooter implements Subsystem {
@@ -11,81 +11,70 @@ public class Shooter implements Subsystem {
 
     private DcMotor shooter;
 
-    private final double SLOW = 0.3;
-    private final double MEDIUM = 0.35;
-    private final double FAST = 0.4;
-    private final double OFF = 0;
-    private final double MAX_SPEED = 1;
-
-    private double      power = OFF;
-    private boolean     reverse = false;
+    private double      power = POWER_LEVELS.FAR.getPower();
     private boolean     shoot = false;
 
-    public enum POWER_LEVELS{
-        MAX,FAR,MEDIUM,SHORT,MIN,
-        ADJ;
+    public enum POWER_LEVELS {
+        FAR,
+        SHORT,
+        POWER_SHOT;
 
         public double getPower() {
-            switch (this){
-                case MAX: return 1.0;
-                case MIN: return 0.0;
+            switch (this) {
                 case FAR: return 0.85;
-                case MEDIUM: return 0.75;
-                case SHORT: return 0.60;
-                case ADJ: return 0.70;
-                default: return 0.75;
+                case SHORT: return 0.75;
+                case POWER_SHOT: return 0.60;
+                default: return 0.85;
             }
         }
     }
 
     public Shooter(HardwareMap hardwareMap){
         this.hardwareMap = hardwareMap;
-
     }
 
     @Override
-    public void initHardware(){
+    public void initHardware() {
         shooter = hardwareMap.get(DcMotor.class, "shooter");
+        shooter.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     @Override
     public void periodic() {
-        shooter.setPower(power * (reverse?-1:1) * (shoot?1:0));
+        shooter.setPower(power * (shoot ? 1 : 0));
     }
 
-    public void setPower(double power){
+    public void setPower(double power) {
         this.power = power;
     }
 
-    public void reversePower(boolean reverse){
-        this.reverse = reverse;
+    public void setDirection(DcMotorSimple.Direction direction) {
+        shooter.setDirection(direction);
     }
 
     public void setShoot(boolean shoot) {
         this.shoot = shoot;
     }
 
-    //TODO: Finish this
-    public void shoot(){
-        this.shoot = !this.shoot;
-    }
-
-    //TODO: Finish this
-    public void reverse(){
-        this.reverse = !this.reverse;
+    public void reverse() {
+        if(shooter.getDirection() == DcMotorSimple.Direction.FORWARD) shooter.setDirection(DcMotorSimple.Direction.REVERSE);
+        else shooter.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     public double getPower() {
         return power;
     }
 
-    public boolean isReverse() {
-        return reverse;
+    public DcMotorSimple.Direction getReverse() {
+        return shooter.getDirection();
     }
 
     public boolean getShoot() {
         return shoot;
     }
 
-
+    //TODO: Finish this
+    public void shoot() {
+        this.shoot = !this.shoot;
+    }
 }
