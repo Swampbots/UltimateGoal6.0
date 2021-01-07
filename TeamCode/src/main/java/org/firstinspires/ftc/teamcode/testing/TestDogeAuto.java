46a0a809ca62dce3dea670ff5af63ffc42ea5746
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2021-01-06 23:10:48 -0500

    Test class for Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestDogeAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestDogeAuto.java
@@ -6,7 +6,16 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.robot.commands.auto.DriveByTimer;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.GripSetState;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.KickerSetState;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.RunShooterForTime;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Drive;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Grip;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Kicker;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Shooter;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Transfer;
 
 
 @Autonomous
@@ -14,18 +23,48 @@ public class TestDogeAuto extends LinearOpMode implements DogeOpMode {
     public void runOpMode(){
         DogeCommander commander = new DogeCommander(this);
 
-        Drive drive             = new Drive(hardwareMap, true);
+        Drive drive             = new Drive(hardwareMap,true);
+        Kicker kicker           = new Kicker(hardwareMap);
+        Shooter shooter         = new Shooter(hardwareMap);
+        Arm arm                 = new Arm(hardwareMap);
+        Grip grip               = new Grip(hardwareMap);
+        Intake intake           = new Intake(hardwareMap);
+        Transfer transfer       = new Transfer(hardwareMap);
 
         commander.registerSubsystem(drive);
+        commander.registerSubsystem(kicker);
+        commander.registerSubsystem(shooter);
+        commander.registerSubsystem(arm);
+        commander.registerSubsystem(grip);
+        commander.registerSubsystem(intake);
+        commander.registerSubsystem(transfer);
 
         commander.init();
 
+        // Run before start
+//        commander.runCommand(new GripSetState(grip, Grip.TARGETS.CLOSE.getTarget()));
+//        commander.runCommand(new KickerSetState(kicker, Kicker.TARGETS.IN.getTarget(), 10.0));
+
+        telemetry.addLine("ready");
+        telemetry.update();
+
         waitForStart();
 
-        commander.runCommandsParallel(
-                new DriveByTimer(drive, 1, 0.3)
+        telemetry.addLine("after start");
+        telemetry.update();
 
+        commander.runCommandsParallel(
+                new RunShooterForTime(shooter,false, 0.75),
+                new DriveByTimer(drive,1,-0.3, telemetry)
         );
+        telemetry.addLine("after drive");
+        telemetry.update();
+//        commander.runCommand(new DriveByTimer(drive,0.1,0));
+//
+//        telemetry.addLine("after 2nd drive");
+//        telemetry.update();
+
+        sleep(2000);
 
 
 
