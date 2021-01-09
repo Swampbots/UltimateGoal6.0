67b17db8b233dce052707c7e1928dd75d7e8dad7
Blake Sanders <ac3.train3r.blak3@gmail.com>
2021-01-09 14:38:15 -0500

    Update TeleOp Arm controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/teleop/TeleOpArmControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/teleop/TeleOpArmControl.java
@@ -6,10 +6,14 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
 
+import static org.firstinspires.ftc.teamcode.CommandDrive.TRIGGER_THRESHOLD;
+
 public class TeleOpArmControl implements Command {
     private Gamepad gamepad;
 
     private Arm wobble;
+
+    private final double POWER_SCALAR = 0.8;
 
     public TeleOpArmControl(Arm wobble, Gamepad gamepad){
         this.gamepad = gamepad;
@@ -25,30 +29,21 @@ public class TeleOpArmControl implements Command {
 
     @Override
     public void periodic() {
-//        if(gamepad.dpad_up){
-//            wobble.setTargetPos(Arm.TARGETS.UP.getTarget());
-//        }
-//        if(gamepad.dpad_down){
-//            wobble.setTargetPos(Arm.TARGETS.DOWN.getTarget());
-//        }
-//        if(gamepad.dpad_left){
-//            wobble.setTargetPos(Arm.TARGETS.OUT.getTarget());
-//        }
-//        if(gamepad.dpad_right){
-//            wobble.resetTargetPos();
-//        }
-//
-//
-//        if(gamepad.a){
-//            wobble.setPower(.2);
-//        } else {
-//            wobble.setPower(0);
-//        }
-        double powerScalar = 0.8;
-        double wobblePower = (gamepad.left_trigger - gamepad.right_trigger);
-        wobble.setPower(powerScalar * wobblePower);
+        // In (encoder): RT     Out (encoder): LT   Up (power): dUp     Down (power): dDown
+        if(gamepad.left_trigger > TRIGGER_THRESHOLD || gamepad.right_trigger > TRIGGER_THRESHOLD) {
+            wobble.setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
+            wobble.setPower(POWER_SCALAR);
 
+            if(gamepad.left_trigger > TRIGGER_THRESHOLD)        wobble.setTargetPos(Arm.TARGETS.OUT.getTarget());
+            else if(gamepad.right_trigger > TRIGGER_THRESHOLD)  wobble.setTargetPos(Arm.TARGETS.UP.getTarget());
+        }
 
+        if(gamepad.dpad_up || gamepad.dpad_down) {
+            wobble.setRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            if(gamepad.dpad_up)         wobble.setPower(POWER_SCALAR);
+            else if(gamepad.dpad_down)  wobble.setPower(-POWER_SCALAR);
+            }
     }
 
     @Override
