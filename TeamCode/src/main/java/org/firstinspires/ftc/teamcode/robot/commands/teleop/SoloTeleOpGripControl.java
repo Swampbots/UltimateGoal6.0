424392c929a513235223c0ebcfba56a8ad5a2ee7
Blake Sanders <ac3.train3r.blak3@gmail.com>
2021-02-14 15:38:02 -0500

    Updated implementation of solo Gripper controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/teleop/SoloTeleOpGripControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/teleop/SoloTeleOpGripControl.java
@@ -3,32 +3,56 @@ package org.firstinspires.ftc.teamcode.robot.commands.teleop;
 import com.disnodeteam.dogecommander.Command;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.firstinspires.ftc.teamcode.CommandDrive;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Grip;
 
 public class SoloTeleOpGripControl implements Command {
-    private TeleOpGripControl gripControl;
+    private Grip grip;
+    private Gamepad gamepad;
 
-    public SoloTeleOpGripControl(Grip grip, Gamepad gamepad) {
-        gripControl = new TeleOpGripControl(grip, gamepad);
+    private boolean gripToggleCheck;
+    public static boolean tellArmGipToggled = false;
+    public static boolean gripOpen;
+
+    public SoloTeleOpGripControl(Grip grip, Gamepad gamepad){
+        this.gamepad = gamepad;
+        this.grip = grip;
     }
 
     @Override
     public void start() {
-        gripControl.start();
+        grip.close();
     }
 
     @Override
     public void periodic() {
-        gripControl.periodic();
+        gripOpen = grip.getCurrentPos() == Grip.TARGETS.OPEN.getTarget();
+
+        if(TeleOpArmControl.tellGripToToggle && !tellArmGipToggled) {
+            grip.open();
+            tellArmGipToggled = true;
+        }
+        if(!TeleOpArmControl.tellGripToToggle) {
+            tellArmGipToggled = false;
+        }
+
+        if(gamepad.a && gripToggleCheck) {
+            grip.togglePos();
+            gripToggleCheck = false;
+        } else if(!gamepad.a && !gripToggleCheck){
+            gripToggleCheck = true;
+        }
+
+
     }
 
     @Override
     public void stop() {
-        gripControl.stop();
+
     }
 
     @Override
     public boolean isCompleted() {
-        return gripControl.isCompleted();
+        return false;
     }
 }
